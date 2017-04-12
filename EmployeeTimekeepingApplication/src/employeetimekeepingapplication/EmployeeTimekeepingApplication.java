7fe5916029df1a553d0db2a9f005313a82e62620
adityatornala <adityatornala@adityas-mbp.fios-router.home>
2017-04-12 01:36:35 -0400

    Add new content

--- EmployeeTimekeepingApplication/src/employeetimekeepingapplication/EmployeeTimekeepingApplication.java
+++ EmployeeTimekeepingApplication/src/employeetimekeepingapplication/EmployeeTimekeepingApplication.java
@@ -5,15 +5,95 @@
  */
 package employeetimekeepingapplication;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Dave
  */
 public class EmployeeTimekeepingApplication {
 
-    public static void main(String[] args) {
-        //create a singly linked list
-        SLinkedList list = new SLinkedList();
+    public static void main(String[] args){
+        LinkedList<Employee> employeeList = new LinkedList<Employee>();
+        
+        
+    }
+    
+    private static void addEmployee(LinkedList<Employee> list){
+        Employee employee = new Employee();
+        createEmployee(employee);
+        list.add(employee);
+    }
+    
+    private static void createEmployee(Employee employee){
+        employee.setFirstName(JOptionPane.showInputDialog("Enter first name:"));
+        employee.setLastName(JOptionPane.showInputDialog("Enter last name: "));
+        employee.setPassword(JOptionPane.showInputDialog("Enter password: "));
+        
+        if(employee instanceof Manager){
+            ///
+        }
+        if(employee instanceof Administrator){
+            //
+        }
+    }
+    
+    private static void addAddress(Employee employee){
+        employee.getAddress().setStreet(JOptionPane.showInputDialog("Enter Street name: "));
+        employee.getAddress().setAptNum(JOptionPane.showInputDialog("Enter apt num: "));
+        employee.getAddress().setCity(JOptionPane.showInputDialog("Enter City: "));
+        employee.getAddress().setState(JOptionPane.showInputDialog("Enter State: "));
+        employee.getAddress().setZipCode(JOptionPane.showInputDialog("Enter ZIP: "));    
+    }
+    
+    private static void addLocation(Employee employee){
+        int id = locationMenu();
+        employee.getLocation().setLocation(Integer.toString(id));
+    }
+    
+    private static int locationMenu(){
+        int locationID;
+        do{
+            try{
+                locationID = Integer.parseInt(JOptionPane.showInputDialog("1. Factory 415, 123 Fake Street, Nowhere, NN, 00111 \n" +  
+                "2. Research 221, 123 Faker Street, Still Nowhere, NN, 00112 \n" + 
+                "3. Corporate 001, 123 Not-Fake Street, Somewhere, IL, 01010"));
+            }
+            catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "Invalid location try again");
+                locationID = -1;
+            }
+        }while(locationID < 0 || locationID > 3 );
+        
+        return locationID;
+    }
+    
+    private static String checkFileExists(String filepath){
+        String fileName = "database.txt";
+        File f = new File(fileName);
+        boolean check = f.exists();
+        
+        if(check == false){
+            try{
+                PrintWriter file = new PrintWriter("database.txt");
+                file.write("Conents");
+                
+            }catch(IOException e){
+            }
+        }
+        
+        return fileName;
+    }
+    
+    private static void login(String filepath){
+        
+    }
+    
+    private static void writeToFile(String filepath){
         
     }
     
